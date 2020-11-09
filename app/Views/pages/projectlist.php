43a61ea9521c6e722d0018886d630feda254165c
windiwidia <windiwindi25@gmail.com>
2020-11-09 10:37:00 +0700

    merubah margin-top dan padding top di project/pages dan menambahkan judul di project/pages

--- app/Views/pages/projectlist.php
+++ app/Views/pages/projectlist.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layout/layout_homepage'); ?>
 <?= $this->section('content_homepage'); ?>
 <div class="container pt-5">
-    <h1 class="text-center pt-3 mt-5">Projects List</h1>
+    <h1 class="text-center pt-5">Projects List</h1>
     <div class="row pt-5 mb-5">
         <?php $i = 1;
         foreach ($projects as $p) : ?>
--- app/Views/pages/projects.php
+++ app/Views/pages/projects.php
@@ -1,13 +1,18 @@
 <?= $this->extend('layout/layout_homepage'); ?>
 <?= $this->section('content_homepage'); ?>
 
-<div class="container pb-5">
-    <div class="card" style="width: 70rem;">
-        <img src="/img/projects/<?= $projects['image']; ?>" class="card-img-top" style="width: 70rem;">
-        <div class="card-body">
-            <p class="card-text"><b>Klien : </b><?= $projects['klien']; ?></p>
-            <p class="card-text"><?= $projects['alamat']; ?></p>
-            <p class="card-text"><?= $projects['deskripsi']; ?></p>
+<div class="container pt-5 pb-5">
+    <h1 class="mt-5 text-center">Proyek <?= $projects['klien']; ?></h1>
+    <div class="row mt-5">
+        <div class="col">
+            <div class="card" style="width: 70rem;">
+                <img src="/img/projects/<?= $projects['image']; ?>" class="card-img-top" style="width: 70rem;">
+                <div class="card-body">
+                    <p class="card-text"><b>Klien : </b><?= $projects['klien']; ?></p>
+                    <p class="card-text"><?= $projects['alamat']; ?></p>
+                    <p class="card-text"><?= $projects['deskripsi']; ?></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
