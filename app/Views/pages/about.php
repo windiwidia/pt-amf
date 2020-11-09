c1f69950a8b119c527225ba5ffaf9ee3be758d70
windiwidia <windiwindi25@gmail.com>
2020-11-09 08:32:59 +0700

    menambahkan content history, visi dan misi di about/pages

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -8,8 +8,44 @@
     </div><!-- /.row -->
     <div class="row">
         <div class="col">
-
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut est possimus fugiat excepturi sequi numquam unde illo quis dicta voluptatum temporibus nulla, consectetur consequuntur nostrum repudiandae quasi ea corporis cupiditate!</p>
+            <h3>HISTORY</h3>
+        </div>
+        <div class="col">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut est possimus fugiat excepturi
+                sequi numquam unde illo quis dicta voluptatum temporibus nulla, consectetur consequuntur
+                nostrum repudiandae quasi ea corporis cupiditate! Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Reprehenderit dolore eos ipsum praesentium culpa ipsa ullam asperiores
+                voluptatibus architecto recusandae inventore quibusdam, quis laudantium? Maiores quia nulla
+                enim molestiae provident.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h3>VISI</h3>
+        </div>
+        <div class="col">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut est possimus fugiat excepturi
+                sequi numquam unde illo quis dicta voluptatum temporibus nulla, consectetur consequuntur
+                nostrum repudiandae quasi ea corporis cupiditate! Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Reprehenderit dolore eos ipsum praesentium culpa ipsa ullam asperiores
+                voluptatibus architecto recusandae inventore quibusdam, quis laudantium? Maiores quia nulla
+                enim molestiae provident.
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h3>MISI</h3>
+        </div>
+        <div class="col">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut est possimus fugiat excepturi
+                sequi numquam unde illo quis dicta voluptatum temporibus nulla, consectetur consequuntur
+                nostrum repudiandae quasi ea corporis cupiditate! Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Reprehenderit dolore eos ipsum praesentium culpa ipsa ullam asperiores
+                voluptatibus architecto recusandae inventore quibusdam, quis laudantium? Maiores quia nulla
+                enim molestiae provident.
+            </p>
         </div>
     </div>
 </div><!-- /.container-fluid -->
