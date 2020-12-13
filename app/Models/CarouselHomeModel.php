634ec397b9d1a04625415caea09efaa74a1ba5e8
windiwidia <windiwindi25@gmail.com>
2020-12-13 21:49:26 +0700

    menambahkan isi untuk file CarouselHomeModel.php

---
+++ app/Models/CarouselHomeModel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CarouselHomeModel extends Model
+{
+    protected $table = 'carouselhome';
+    // protected $useTimestamps = true;
+    protected $allowedFields = ['slug', 'image', 'title', 'description'];
+
+    // supaya lebih rapi kita gabungkan kedalam method sndiri di dalam models
+    // getProjects ini bisa jalanin 2 jika ada parameter cari menggunakan where, jika tidak ada ambil semua data projects 
+    public function getCarouselHome($slug = false)
+    {
+        // jika slugnya false maka ditampilkkan this projectsModel lalu find all
+        if ($slug == false) {
+            return $this->findAll();
+        }
+        // jikada return 
+        return $this->where(['slug' => $slug])->first();
+    }
+}
