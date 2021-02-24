bf1d829909fe772bf9cdf5bc26edcfb64caf9faa
windiwidia <windiwindi25@gmail.com>
2021-02-24 17:47:49 +0700

    modified carouselhome

--- app/Controllers/CarouselHome.php
+++ app/Controllers/CarouselHome.php
@@ -86,13 +86,7 @@ class Carouselhome extends BaseController
             //pindahkan file ke folder img
             $fileImage->move('img/carouselhome', $namaImage);
         }
-        // ambil nama file dengan nama yang sama saat upload
-        // $namaImage = $fileImage->getName();
 
-        // cara ngambil data yang dikirim, getvar mengambil request apapun get atau post
-        // dd($this->request->getVar());
-        // insert ke dalam database
-        // string ramah url = url_title untuk menjadi hrufnya kecil semua dan spasinya hilang 
         $slug = url_title($this->request->getVar('title'), '-', true);
         $this->carouselhomeModel->save([
             'image' => $namaImage,
@@ -102,6 +96,102 @@ class Carouselhome extends BaseController
         ]);
         // sebelum redirect kita bikin dlu flash datanya
         session()->setFlashdata('pesan', 'Data berhasil ditambahkan.');
+
+        // kalo sudah ngesave kita kembalikan ke halaman daftar projects
+        return redirect()->to('/carouselhome');
+    }
+
+    public function delete($id)
+    {
+
+        $this->carouselhomeModel->delete($id);
+        session()->setFlashdata('pesan', 'Data berhasil dihapus.');
+        return redirect()->to('/carouselhome');
+    }
+
+    public function edit($slug)
+    {
+        $data = [
+            'data' => $this->authModel->where(['email' => session()->get('email')])->first(),
+            'title' => 'Form Ubah Carousel Home',
+            'carouselhome' => $this->carouselhomeModel->getCarouselHome($slug),
+            'validation' => \Config\Services::validation(),
+            // 'client' => $this->clientModel->getClient($slug)
+        ];
+
+        return view('carouselhome/edit', $data);
+    }
+
+    public function update($id)
+    {
+        // cek judul 
+        $titleLama =  $this->carouselhomeModel->getCarouselHome($this->request->getVar('slug'));
+        if ($titleLama['title'] == $this->request->getVar('title')) {
+            $rule_title = 'required';
+        } else {
+            $rule_title = 'required';
+        }
+
+
+        if (!$this->validate([
+            'image' => [
+                'rules' => 'max_size[image, 2048]|is_image[image]|mime_in[image,image/jpg,image/jpeg,image/png]',
+                'errors' => [
+                    'max_size' => 'Ukuran gambar terlalu besar',
+                    'is_image' => 'Yang anda pilih bukan gambar',
+                    'mime_in' => 'Yang anda pilih bukan gambar'
+                ]
+            ],
+            'title' => [
+                'rules' => $rule_title,
+                'errors' => [
+                    'required' => '{field} harus diisi.'
+                    // 'is_unique' => '{field} sudah terdaftar.'
+                ]
+            ],
+            'description' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} harus diisi.'
+                    // 'is_unique' => '{field} sudah terdaftar.'
+                ]
+            ]
+        ])) {
+
+            return redirect()->to('/carouselhome/edit/' . $this->request->getVar('slug'))->withInput();
+        }
+
+        // ambil gambar 
+        $fileImage = $this->request->getFile('image');
+
+        // cek gambar apakah gambarnya berubah atau pake gambar yang lama/ apakah tidak ada gambar yang di upload
+        if ($fileImage->getError() == 4) {
+            // $namaImage = 'default'
+            $namaImage = $this->request->getVar('imageLama');
+        } else {
+            // generate nama file random / ambil nama file
+            $namaImage = $fileImage->getRandomName();
+
+            // pindahkan gambar / upload gambar ke folder img
+            $fileImage->move('img/carouselhome/', $namaImage);
+
+            if ($this->request->getVar('imageLama') != 'default.jpg') {
+
+                // hapus file lama
+                unlink('img/carouselhome/' . $this->request->getVar('imageLama'));
+            }
+        }
+        // dd($this->request->getVar());
+        $slug = url_title($this->request->getVar('title'), '-', true);
+        $this->carouselhomeModel->save([
+            'id' => $id,
+            'image' => $this->request->getVar('image'),
+            'title' => $this->request->getVar('title'),
+            'slug' => $slug,
+            'description' => $this->request->getVar('description')
+        ]);
+        // sebelum redirect kita bikin dlu flash datanya
+        session()->setFlashdata('pesan', 'Data berhasil diubah.');
 
         // kalo sudah ngesave kita kembalikan ke halaman daftar projects
         return redirect()->to('/carouselhome');
